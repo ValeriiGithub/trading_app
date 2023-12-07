7563b59755bd36d0a8327f512bf628873d5d2d4e
Valerii <valeri.github@gmail.com>
2023-12-07 16:23:32 +0300

    Добавить fake_trades

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ fake_users = [
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
