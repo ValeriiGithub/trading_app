7187c14fd9c7276f66990da79e6325dd66db543c
Valerii <valeri.github@gmail.com>
2023-12-07 16:25:47 +0300

    Создать эндпоинт get_trades

--- main.py
+++ main.py
@@ -14,12 +14,19 @@ fake_users = [
     {"id": 3, "role": "trader", "name": "Matt"},
 ]
 
+
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
+
 
 fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
 
+
+# эндпоинт
+@app.get("/trades")
+def get_trades(limit: int, offset: int):        # offset это сдвиг в пагинации
+    return fake_trades[offset:][:limit]
