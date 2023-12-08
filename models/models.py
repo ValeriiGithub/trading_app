97ca85af900708aa7ecd410ce36e36db2443a962
Valerii <valeri.github@gmail.com>
2023-12-08 16:45:32 +0300

    Создать модель БД таблицу users

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
 
 metadata = MetaData()
@@ -8,4 +10,15 @@ roles = Table(
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permissions", JSON),
+)
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("usrname", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey("roles.id")),
 )
