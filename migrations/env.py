75bc9d15bca0b8e7ed3756ad7399236e68e3f246
Valerii <valeri.github@gmail.com>
2023-12-10 15:12:43 +0300

    Импортировать в alevbic данные для подключения к БД

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,18 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", DB_HOST)
+config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_USER", DB_USER)
+config.set_section_option(section, "DB_NAME", DB_NAME)
+config.set_section_option(section, "DB_PASS", DB_PASS)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
