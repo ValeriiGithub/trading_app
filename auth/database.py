95bb45c2a0bed37b6bd29f18ab4d093eda95b48c
Valerii <valeri.github@gmail.com>
2023-12-11 18:19:02 +0300

    Исправить БАГ

--- auth/database.py
+++ auth/database.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from models.models import role
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base: DeclarativeMeta = declarative_base()
@@ -19,7 +20,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     email = Column(String, nullable=False),
     username = Column(String, nullable=False),
     registered_at = Column(TIMESTAMP, default=datetime.utcnow),
-    role_id = Column(Integer, ForeignKey(roles.c.id)),
+    role_id = Column(Integer, ForeignKey(role.c.id)),
 
     # Скопировано автором из SQLAlchemyBaseUserTable. Возможно у него старая версия FastAPI
     email: str = Column( String(Length=328), unique=True, index=True, nullable=False),
