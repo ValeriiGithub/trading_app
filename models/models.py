fd01f2eb957aea7b758b17309524fe4ae1542d94
Valerii <valeri.github@gmail.com>
2023-12-11 18:21:04 +0300

    Исправить название поля password

--- models/models.py
+++ models/models.py
@@ -18,7 +18,7 @@ user = Table(
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
     Column("role_id", Integer, ForeignKey(role.c.id)),
 )
