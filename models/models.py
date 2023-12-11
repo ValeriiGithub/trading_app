da7e402722fa9817aaf6b2f66cd0a6a6442f2eff
Valerii <valeri.github@gmail.com>
2023-12-11 18:07:40 +0300

    Исправить БАГ опечатка

--- models/models.py
+++ models/models.py
@@ -17,7 +17,7 @@ users = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
-    Column("usrname", String, nullable=False),
+    Column("username", String, nullable=False),
     Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
     Column("role_id", Integer, ForeignKey("roles.id")),
