6bcce0c62433c2befbdec1fc123089c0c45a46f9
Valerii <valeri.github@gmail.com>
2023-12-13 14:30:04 +0300

    Модифицировать файл с cookie

--- auth/auth.py
+++ auth/auth.py
@@ -1,3 +1,3 @@
 from fastapi_users.authentication import CookieTransport
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
