fe06927d74323e2a389a0f856f2a0bcfc6ca7aad
Valerii <valeri.github@gmail.com>
2023-12-13 14:38:39 +0300

    Заменить UUIDIDMixin

--- auth/manager.py
+++ auth/manager.py
@@ -2,14 +2,14 @@ import uuid
 from typing import Optional
 
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users import BaseUserManager, IntegerIDMixin
 
 from database import User, get_user_db
 
 SECRET = "SECRET"
 
 
-class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+class UserManager(IntegerIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
 
