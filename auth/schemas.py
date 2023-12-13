a12d679763f525976c5236e47c382361d62ecf25
Valerii <valeri.github@gmail.com>
2023-12-13 14:43:55 +0300

    Создать показать пользователя

--- auth/schemas.py
+++ auth/schemas.py
@@ -4,7 +4,13 @@ from fastapi_users import schemas
 
 
 class UserRead(schemas.BaseUser[int]):
-    pass
+    id: int
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
 
 
 class UserCreate(schemas.BaseUserCreate):
