41cfc5c94dea45efd0c8d608b7d27f7517942f53
Valerii <valeri.github@gmail.com>
2023-12-13 14:25:07 +0300

    PEP8

--- auth/database.py
+++ auth/database.py
@@ -33,7 +33,6 @@ engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
