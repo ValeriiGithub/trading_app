ae5940ce66c473590844e30ca44622dc0423d62f
Valerii <valeri.github@gmail.com>
2023-12-13 14:36:01 +0300

    Исправить БАГ

--- auth/manager.py
+++ auth/manager.py
@@ -4,7 +4,7 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, UUIDIDMixin
 
-from .db import User, get_user_db
+from database import User, get_user_db
 
 SECRET = "SECRET"
 
