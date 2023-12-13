6531adb8d4e7550cbea4e6cf1b6003f6ef3d7e86
Valerii <valeri.github@gmail.com>
2023-12-13 14:56:20 +0300

    Создать интстансе fastapi_users

--- main.py
+++ main.py
@@ -3,13 +3,20 @@
 # http://127.0.0.1:8000/redoc - менее удобный
 
 from fastapi import FastAPI
-from fastapi_users import fastapi_users
+from fastapi_users import FastAPIUsers
 
 from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
 from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
 )
 
 app.include_router(
