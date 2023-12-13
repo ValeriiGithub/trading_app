ea9314d4e600ea98b01c1ae1e313deb7e33e3e9a
Valerii <valeri.github@gmail.com>
2023-12-13 14:42:41 +0300

    Создать UserCreate

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi_users import schemas
 
 
@@ -6,8 +8,12 @@ class UserRead(schemas.BaseUser[int]):
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    username: str
+    email: str
+    password: str
+    role_id: int
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
 
 
-class UserUpdate(schemas.BaseUserUpdate):
-    pass
