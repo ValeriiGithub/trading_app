3da5e25217b87dadc5748d28d320ef1bbeaaca62
Valerii <valeri.github@gmail.com>
2023-12-13 14:53:29 +0300

    Создать роутер для регистрации

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi_users import fastapi_users
 
 from auth.auth import auth_backend
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
@@ -14,5 +15,11 @@ app = FastAPI(
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
     tags=["auth"],
 )
