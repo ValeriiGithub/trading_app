75d36bb19e0fe371ac519284b5b1b8ccd9b26cab
Valerii <valeri.github@gmail.com>
2023-12-13 14:34:21 +0300

    Создать переменную ayth_backend

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,4 @@
-from fastapi_users.authentication import CookieTransport
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
 from fastapi_users.authentication import JWTStrategy
 
 cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
@@ -8,3 +8,9 @@ SECRET = "SECRET"   # неиобходимо данный секрет импо�
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
