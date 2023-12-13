165b6f712cd06143c8c541610288656a505d9680
Valerii <valeri.github@gmail.com>
2023-12-13 14:32:33 +0300

    Добавить комментарий

--- auth/auth.py
+++ auth/auth.py
@@ -3,7 +3,7 @@ from fastapi_users.authentication import JWTStrategy
 
 cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
 
-SECRET = "SECRET"
+SECRET = "SECRET"   # неиобходимо данный секрет импортировать из .env
 
 
 def get_jwt_strategy() -> JWTStrategy:
