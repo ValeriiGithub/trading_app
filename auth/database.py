c10914f1e843893b4efcc961ab086305ab20ad91
Valerii <valeri.github@gmail.com>
2023-12-13 14:24:19 +0300

    Удалить функцию создания таблиц

--- auth/database.py
+++ auth/database.py
@@ -33,10 +33,6 @@ engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
