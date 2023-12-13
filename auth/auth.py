ca4879c0191393f7c0de9efd935e12b3e27fd39a
Valerii <valeri.github@gmail.com>
2023-12-13 14:31:21 +0300

    Создать JWT токенезацию

--- auth/auth.py
+++ auth/auth.py
@@ -1,3 +1,10 @@
 from fastapi_users.authentication import CookieTransport
+from fastapi_users.authentication import JWTStrategy
 
 cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
+
+SECRET = "SECRET"
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
