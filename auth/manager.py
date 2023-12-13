440e0c0fd79c27ecccc6a59f8edd016178fc1952
Valerii <valeri.github@gmail.com>
2023-12-13 14:37:28 +0300

    Удалить функции с которыми не будем работать

--- auth/manager.py
+++ auth/manager.py
@@ -16,16 +16,6 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
 
-    async def on_after_forgot_password(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        print(f"User {user.id} has forgot their password. Reset token: {token}")
-
-    async def on_after_request_verify(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        print(f"Verification requested for user {user.id}. Verification token: {token}")
-
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
