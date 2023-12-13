e9de0ea9653601ba60c901dfcbebe857f3198f5e
Valerii <valeri.github@gmail.com>
2023-12-13 14:52:37 +0300

    Создать роутер

--- main.py
+++ main.py
@@ -3,43 +3,16 @@
 # http://127.0.0.1:8000/redoc - менее удобный
 
 from fastapi import FastAPI
+from fastapi_users import fastapi_users
+
+from auth.auth import auth_backend
 
 app = FastAPI(
     title="Trading App"
 )
 
-fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
-
-
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
-]
-
-
-# эндпоинт
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    # offset это сдвиг в пагинации
-    return fake_trades[offset:][:limit]
-
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
