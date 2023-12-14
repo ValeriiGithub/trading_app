f00775c528a889fdc83a7fd74bafa769c0c1ab57
Valerii <valeri.github@gmail.com>
2023-12-14 17:54:54 +0300

    Добавить путь для alembic к папке src (без подробностей((()

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
 from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 from src.auth.models import metadata as metadata_auth
 from src.operations.models import metadata as metadata_operations
