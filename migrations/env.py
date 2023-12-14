09efff7a662c801ea1da280c4201b255e080fd5a
Valerii <valeri.github@gmail.com>
2023-12-14 17:56:26 +0300

    Добавить метадаты для alembic чтобы создавать миграции

--- migrations/env.py
+++ migrations/env.py
@@ -35,7 +35,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 # target_metadata = None
-target_metadata = metadata
+target_metadata = [metadata_auth, metadata_operations]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
