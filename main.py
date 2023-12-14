a40484731ac9a6d4d4ba8ab57e4b4709051592a0
Valerii <valeri.github@gmail.com>
2023-12-14 17:30:04 +0300

    Изменить импорты

--- main.py
+++ main.py
@@ -5,10 +5,10 @@
 from fastapi import FastAPI, Depends
 from fastapi_users import FastAPIUsers
 
-from auth.auth import auth_backend
-from auth.database import User
-from auth.manager import get_user_manager
-from auth.schemas import UserRead, UserCreate
+from src.auth.auth import auth_backend
+from src.auth.database import User
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
--- src/auth/manager.py
+++ src/auth/manager.py
@@ -3,7 +3,7 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
-from auth.database import User, get_user_db
+from src.auth.database import User, get_user_db
 
 SECRET = "SECRET"
 
