04c7610bccf41d42054dd5ab5a423dae61bfa227
Valerii <valeri.github@gmail.com>
2023-12-14 17:52:26 +0300

    Добавить импорты метаданных

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from src.auth.models import metadata
+from src.auth.models import metadata as metadata_auth
+from src.operations.models import metadata as metadata_operations
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
