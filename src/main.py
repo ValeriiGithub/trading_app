3eff2cc36646b5627c4c1a9570d52233b5f9882a
Valerii <valeri.github@gmail.com>
2023-12-15 10:13:49 +0300

    Изменить в соответсвии с учебным файлом

--- src/main.py
+++ src/main.py
@@ -2,21 +2,13 @@
 # http://127.0.0.1:8000/docs - обращение через swagger
 # http://127.0.0.1:8000/redoc - менее удобный
 
-from fastapi import FastAPI, Depends
-from fastapi_users import FastAPIUsers
+from fastapi import FastAPI
 
-from src.auth.auth import auth_backend
-from src.database import User
-from src.auth.manager import get_user_manager
-from src.auth.schemas import UserRead, UserCreate
+from auth.base_config import auth_backend, fastapi_users
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
-)
-
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
 )
 
 app.include_router(
@@ -30,15 +22,3 @@ app.include_router(
     prefix="/auth",
     tags=["auth"],
 )
-
-current_user = fastapi_users.current_user()
-
-
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
-
-
-@app.get("/unprotected-route")
-def unprotected_route():
-    return f"Hello, anonym"
